a28bfe39216e3b9bafb68a7d23bc334fd015e81a
Kimberly Wong <Kimberly@kimberlys-macbook-pro.local>
2015-12-20 13:31:18 -0800

    Initial Commit

---
+++ TipCalculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TipCalculator
+//
+//  Created by Kimberly Wong on 12/20/15.
+//  Copyright © 2015 Kimberly Wong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
